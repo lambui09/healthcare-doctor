7977f75a4d7490630e6e2622c43ad20cd379ecb9
Bui Duc Lam <buiduclam09@gmail.com>
2020-06-28 13:37:55 +0700

    update ui time appointment

--- app/src/main/java/com/lambui/healthcare_doctor/ui/main/appointment/appointmentPending/AppointmentPendingAdapter.kt
+++ app/src/main/java/com/lambui/healthcare_doctor/ui/main/appointment/appointmentPending/AppointmentPendingAdapter.kt
@@ -12,32 +12,35 @@ import com.lambui.healthcare_doctor.utils.extension.listen
 import com.lambui.healthcare_doctor.utils.extension.loadImageUrl
 import kotlinx.android.synthetic.main.item_view_patient_horizental_appointment.view.*
 
-class AppointmentPendingAdapter(context: Context) : BaseLoadMoreAdapter<AppointmentFullModel>(context) {
-    override fun onCreateViewHolderLM(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val view = LayoutInflater.from(context)
-            .inflate(R.layout.item_view_patient_horizental_appointment, parent, false)
-        return UpcomingVH(view).listen { position, type ->
-            getItem(position)?.let {
-                itemClickListener?.onItemViewClick(it, position)
-            }
-        }
+class AppointmentPendingAdapter(context: Context) :
+  BaseLoadMoreAdapter<AppointmentFullModel>(context) {
+  override fun onCreateViewHolderLM(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    val view = LayoutInflater.from(context)
+      .inflate(R.layout.item_view_patient_horizental_appointment, parent, false)
+    return UpcomingVH(view).listen { position, type ->
+      getItem(position)?.let {
+        itemClickListener?.onItemViewClick(it, position)
+      }
     }
+  }
 
-    override fun onBindViewHolderLM(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as UpcomingVH).bindData(getItem(position))
-    }
+  override fun onBindViewHolderLM(holder: RecyclerView.ViewHolder, position: Int) {
+    (holder as UpcomingVH).bindData(getItem(position))
+  }
 
-    override fun getItemViewTypeLM(position: Int) = 0
+  override fun getItemViewTypeLM(position: Int) = 0
 }
 
 class UpcomingVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    fun bindData(appointmentModel: AppointmentFullModel?) = with(itemView) {
-        with(appointmentModel) {
-            imgProfilePatient.loadImageUrl(appointmentModel?.patientId?.avatar)
-            tvNamePatient.text = appointmentModel?.patientId?.fullName ?: "Bùi Đức Lâm"
-            tvLocationOfPatient.text = appointmentModel?.patientId?.address ?: "updating"
-            tvStatus.text = resources.getText(R.string.text_status_pending)
-            tvStatus.isSelected = false
-        }
+  fun bindData(appointmentModel: AppointmentFullModel?) = with(itemView) {
+    with(appointmentModel) {
+      imgProfilePatient.loadImageUrl(appointmentModel?.patientId?.avatar)
+      tvNamePatient.text = appointmentModel?.patientId?.fullName ?: "Bùi Đức Lâm"
+      tvLocationOfPatient.text = appointmentModel?.patientId?.address ?: "updating"
+      tvTimeAppointment.text =
+        appointmentModel?.timeStartBook + "-" + appointmentModel?.dataStartBook
+      tvStatus.text = resources.getText(R.string.text_status_pending)
+      tvStatus.isSelected = false
     }
+  }
 }
